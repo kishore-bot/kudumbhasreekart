03687eb3f8fa0b99a6a6c702548385ee94b7ee0d
KISHORE K <kishoreabhi222@gmail.com>
2022-09-13 19:26:21 +0530

    uploading server

---
+++ index.js
@@ -0,0 +1,35 @@
+// IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+// INIT
+const PORT = process.env.PORT || 10000;
+const app = express();
+
+const DB="mongodb+srv://kishore:k12345678@cluster0.5we69.mongodb.net/?retryWrites=true&w=majority";
+// const DB="mongodb://localhost:27017";
+// middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
